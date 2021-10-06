d95b4a518ed9f81157e20acddc5e01c8211ecd01
Pattarapon Koomphon <45892157+Kevinindy2k@users.noreply.github.com>
2021-10-06 16:07:03 +0700

    update index

--- index.php
+++ index.php
@@ -7,6 +7,49 @@
     <title>Recommend-Restaurant</title>
 </head>
 <body>
-    
+    <header>
+        <h1>Welcome to my website</h1>
+        <h2>ประวัติส่วนตัว</h2>
+    </header>
+    <article>
+        <header>
+            <h2>ชื่อ : เควิน</h2>
+        </header>
+        <header>
+            <h2>อายุ : 18 ปี</h2>
+        </header>
+        <header>
+            <h2>สิ่งที่ชอบ :</h2>
+        </header>
+        <section>
+        <ui>
+            <li>ราดหน้า</li>
+            <li>แซลม่อน</li>
+            <li>กุ้งทะเล</li>
+        </ui>
+        </section>
+        <header>
+            <h2>สิ่งที่ไม่ชอบ :</h2>
+        </header>
+        <section>
+        <ui>
+            <li>ข้าวผัด</li>
+            <li>ปลาร้า</li>
+        </ui>
+        </section>
+        <header>
+            <h2>กิจกรรมยามว่าง :</h2>
+        </header>
+        <section>
+        <ui>
+            <li>เตะบอล</li>
+            <li>ดูหนัง</li>
+        </ui>
+        </section>
+
+    </article>
+    <footer>
+            <p>สามารถติดต่อผมได้ที่ : <a href="https://www.facebook.com/kevinindy2k/" target="_blank">KeWie</a></p>
+        </footer>
 </body>
 </html>
