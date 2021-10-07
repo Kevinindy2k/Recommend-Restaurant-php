fa1abfb137639af4fb5c44a8788173f378e39228
Pattarapon Koomphon <45892157+Kevinindy2k@users.noreply.github.com>
2021-10-08 00:07:52 +0700

    add new navbar + node modules + clean code

--- index.php
+++ index.php
@@ -4,52 +4,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Recommend-Restaurant</title>
+    <title>Document</title>
+    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
 </head>
 <body>
-    <header>
-        <h1>Welcome to my website</h1>
-        <h2>ประวัติส่วนตัว</h2>
-    </header>
-    <article>
-        <header>
-            <h2>ชื่อ : เควิน</h2>
-        </header>
-        <header>
-            <h2>อายุ : 18 ปี</h2>
-        </header>
-        <header>
-            <h2>สิ่งที่ชอบ :</h2>
-        </header>
-        <section>
-        <ui>
-            <li>ราดหน้า</li>
-            <li>แซลม่อน</li>
-            <li>กุ้งทะเล</li>
-        </ui>
-        </section>
-        <header>
-            <h2>สิ่งที่ไม่ชอบ :</h2>
-        </header>
-        <section>
-        <ui>
-            <li>ข้าวผัด</li>
-            <li>ปลาร้า</li>
-        </ui>
-        </section>
-        <header>
-            <h2>กิจกรรมยามว่าง :</h2>
-        </header>
-        <section>
-        <ui>
-            <li>เตะบอล</li>
-            <li>ดูหนัง</li>
-        </ui>
-        </section>
 
-    </article>
-    <footer>
-            <p>สามารถติดต่อผมได้ที่ : <a href="https://www.facebook.com/kevinindy2k/" target="_blank">KeWie</a></p>
-        </footer>
+<div class ="container">
+container
+</div>
+
+
+<script src="node_modules/jquery/dist/jquery.min.js"></script>
+<script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="node_modules/popper.js/dist/umd/popper.min.js"></script>    
 </body>
 </html>
