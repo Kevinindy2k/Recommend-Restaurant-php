e8f4239ef7fff35b7d79c4fda6e4be94b1b8fc5a
Pattarapon Koomphon <45892157+Kevinindy2k@users.noreply.github.com>
2021-10-16 16:27:11 +0700

    add Blog-Detail + OwlCarousel2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,28 @@
-$( window ).on('scroll', function(){
+$(window).on('scroll', function () {
     var scrollTop = $(this).scrollTop()
     var score = '15px'
     if (scrollTop > 1) {
         score = '5px 15px'
     }
     $('#navbar').css('padding', score)
+});
+
+$(document).ready(function () {
+    $('.owl-carousel').owlCarousel({
+        loop: false,
+        nav: false,
+        dots: true,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            600: {
+                items: 2,
+
+            },
+            1000: {
+                items: 4,
+            }
+        }
+    });
 });
